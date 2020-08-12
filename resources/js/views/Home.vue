ff09a5e8bd13d12543c3f598f18f0cb3ced16237
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-08-12 11:49:46 +0300

    28 - Vue Filters

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -10,7 +10,8 @@
                         </p>
                         <p>
 <!--                            A moment ago ...-->
-                            {{ postedOn(status) }}
+<!--                            {{ postedOn(status) }}-->
+                            {{ status.created_at | ago | capitalise }}   <!--// filter pipe syntax-->
                         </p>
                     </div>
 
@@ -32,6 +33,14 @@ export default {
             statuses: []
         }
     },  // postedOn(status)
+    filters: {
+        ago(date) {
+            return moment(date).fromNow();
+        },
+        capitalise(value) {
+            return value.toUpperCase();
+        }
+    },
     created() {
         // fire off an ajax request to fetch all of the statuses
         // axios.get('/statuses')
@@ -39,10 +48,10 @@ export default {
         // .then(response => this.statuses = response.data);
         // .then(({data}) => this.statuses = data);  // ES2015 collection destructuring
     },
-    methods: {
+/*    methods: {
         postedOn(status) {
-            return moment(status.created_at).fromNow();
+            // return moment(status.created_at).fromNow();
         }
-    }
+    }*/
 }
 </script>
